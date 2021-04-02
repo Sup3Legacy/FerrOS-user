5ce3ae13ad25f7d637a548df5ed5d1b202e8638a
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-04-02 13:25:24 +0200

    fix test_io

--- test_io/src/main.rs
+++ test_io/src/main.rs
@@ -9,7 +9,7 @@ use x86_64::VirtAddr;
 
 #[no_mangle]
 pub extern "C" fn _start() {
-    syscall(0, 0, 0);
+    syscall(0, 0, 0, 0);
     main();
 }
 
@@ -22,9 +22,9 @@ pub fn panic(_: &PanicInfo) -> ! {
 
 #[inline(never)]
 fn main() {
-    syscall(0, 0, 0);
+    syscall(0, 0, 0, 0);
     print("hello world");
-    syscall(2, 0, 0);
+    syscall(2, 0, 0, 0);
     panic!("failure");
 }
 
@@ -34,7 +34,7 @@ fn print(a: &str) {
     let mut index = 0_usize;
     
     for c in a.bytes() {
-        syscall(20, index as u64, c as u64);
+        //syscall(20, index as u64, c as u64, 0);
         t[index] = c;
         index += 1;
         if index == 128 {
@@ -42,47 +42,22 @@ fn print(a: &str) {
             break
         }
     }
-    syscall(1, 0, 0);
-    syscall_write(1, VirtAddr::from_ptr(t.as_ptr()).as_u64(), index as u64);
+    let data_addr = VirtAddr::from_ptr(&t as *const u8);
+    syscall(1, 2, data_addr.as_u64(), index as u64);
 }
 
-extern "C" fn syscall_write(arg0 : u64, arg1 : u64, arg2 : u64) -> usize{
+#[inline(never)]
+extern "C" fn syscall(nb: u64, arg0 : u64, arg1 : u64, arg2 : u64) -> usize{
     let res;
     unsafe {
         asm!(
-            "push rax",
-            "push rdi",
-            "push rsi",
-            "mov rax, 1",
-            "mov rdi, {}", 
-            "mov rsi, {}",
-            "mov rdx, {}",
-            "int 80h",
-            "pop rsi",
-            "pop rdi",
-            "mov {}, rax",
-            "pop rax", 
-            in(reg) arg0, in(reg) arg1, in(reg) arg2, out(reg) res)
-    };
-    res
-}
-
-extern "C" fn syscall(nb: u64, arg0 : u64, arg1 : u64) -> usize{
-    let res;
-    unsafe {
-        asm!(
-            "push rax",
-            "push rdi",
-            "push rsi",
             "mov rax, {}", 
             "mov rdi, {}",
             "mov rsi, {}",
+            "mov rdx, {}",
             "int 80h",
-            "pop rsi",
-            "pop rdi",
             "mov {}, rax", 
-            "pop rax", 
-            in(reg) nb, in(reg) arg0, in(reg) arg1, out(reg) res)
+            in(reg) nb, in(reg) arg0, in(reg) arg1, in(reg) arg2, out(reg) res)
     };
     res
 }

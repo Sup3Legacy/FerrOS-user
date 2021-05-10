c6da3b4aee27dfec4fc9797723040e4c6400c018
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-05-10 22:33:41 +0200

    refactored code for test_listen

--- test_listen/src/main.rs
+++ test_listen/src/main.rs
@@ -1,34 +1,19 @@
 #![no_std]
 #![feature(start)]
-#![feature(asm)]
 #![no_main]
-#![cfg_attr(test, no_main)]
-#![feature(alloc_error_handler)]
-#![feature(custom_test_frameworks)]
-#![feature(core_intrinsics)]
-#![feature(gen_future)]
-#![feature(const_mut_refs)]
-#![feature(naked_functions)]
-#![feature(abi_x86_interrupt)]
-#![feature(intra_doc_pointers)]
 
-use core::panic::PanicInfo;
+use ferr_os_librust::syscall;
 
-
-#[panic_handler]
-fn panic(_panicInfo : &PanicInfo) -> !{
-    panic!("")
-}
 
 #[no_mangle]
-pub extern "C" fn _start(heap_address: u64, heap_size: u64, args: u64) {
+pub extern "C" fn _start(_heap_address: u64, _heap_size: u64, _args: u64) {
     main();
 }
 
 #[inline(never)]
 fn main() {
     loop {
-        let new_id = fork();
+        let new_id = unsafe { syscall::fork() };
         if new_id == 0 {
             return
         }
@@ -38,42 +23,13 @@ fn main() {
 
 fn wait_end() {
     loop {
-        let (rax, rdi) = listen();
+        let (rax, _rdi) = unsafe { syscall::listen() };
         if rax == 0 {
-            sleep();
+            unsafe {
+                syscall::sleep()
+            };
         } else {
             return;
         }
     }
-}
-
-fn fork() -> u64 {
-    syscall(5, 0, 0, 0) as u64
-}
-
-fn listen() -> (usize, usize) {
-    let r1;
-    let r2;
-    unsafe {
-        asm!(
-            "int 80h",
-            in("rax") 22, lateout("rax") r1, lateout("rdi") r2
-        )
-    };
-    (r1, r2)
-}
-
-fn sleep() {
-    syscall(8, 0, 0, 0);
-}
-
-#[inline(never)]
-pub extern "C" fn syscall(nb: u64, arg0: u64, arg1: u64, arg2: u64) -> usize {
-    let res;
-    unsafe {
-        asm!(
-            "int 80h",
-            in("rax") nb, in("rdi") arg0, in("rsi") arg1, in("rdx") arg2, lateout("rax") res)
-    };
-    res
 }

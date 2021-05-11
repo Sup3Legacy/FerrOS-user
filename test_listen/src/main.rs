ad924d328a64e310d54f1e3c0261433e76e8ba0b
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-05-11 08:23:06 +0200

    added printing to test_listen

--- test_listen/src/main.rs
+++ test_listen/src/main.rs
@@ -3,20 +3,34 @@
 #![no_main]
 
 use ferr_os_librust::syscall;
+use ferr_os_librust::io;
 
+
+extern crate alloc;
+use alloc::string::ToString;
 
 #[no_mangle]
-pub extern "C" fn _start(_heap_address: u64, _heap_size: u64, _args: u64) {
+pub extern "C" fn _start(heap_address: u64, heap_size: u64, _args: u64) {
+    ferr_os_librust::allocator::init(heap_address, heap_size);
+    unsafe {
+        syscall::set_screen_size(1, 10);
+        syscall::set_screen_pos(0, 0);
+    }
     main();
 }
 
 #[inline(never)]
 fn main() {
+    let mut compteur = 0_usize;
     loop {
         let new_id = unsafe { syscall::fork() };
+        compteur += 1;
         if new_id == 0 {
             return
         }
+        let mut sortie = compteur.to_string();
+        sortie.push_str("\n");
+        io::print(&sortie);
         wait_end();
     }
 }

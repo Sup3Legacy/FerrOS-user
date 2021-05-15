8b70c5dfb699ca1ba5478ad6d6fdc4d4ceabc408
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-05-15 18:30:39 +0200

    modified launcher

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -6,6 +6,7 @@ use ferr_os_librust::syscall;
 
 extern crate alloc;
 use alloc::string::String;
+use alloc::vec::Vec;
 
 #[no_mangle]
 pub extern "C" fn _start(heap_address: u64, heap_size: u64, _args: u64) {
@@ -13,9 +14,9 @@ pub extern "C" fn _start(heap_address: u64, heap_size: u64, _args: u64) {
         ferr_os_librust::allocator::init(heap_address, heap_size);
         let id = syscall::fork();
         if id == 0 {
-            syscall::exec(String::from("User/root/clock"));
+            syscall::exec(String::from("User/root/clock"), &Vec::new());
         }
         
-        syscall::exec(String::from("User/root/ferr_shell"));
+        syscall::exec(String::from("User/root/ferr_shell"), &Vec::new());
     }
 }

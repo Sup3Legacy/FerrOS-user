b8024c98dee31b24e50570d71975dd7f5a6cf395
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-05-15 22:05:27 +0200

    corrected error

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -10,9 +10,9 @@ use alloc::string::String;
 use alloc::vec::Vec;
 
 #[no_mangle]
-pub extern "C" fn _start(heap_address: u64, heap_size: u64, args_number: u64, _args: u64) {
+pub extern "C" fn _start(heap_address: u64, heap_size: u64, args_number: u64, args: u64) {
     ferr_os_librust::allocator::init(heap_address, heap_size);
-    let arguments = ferr_os_librust::env::retrieve_arguments(args_number, _args);
+    let arguments = ferr_os_librust::env::retrieve_arguments(args, args_number);
     main(arguments);
 }
 

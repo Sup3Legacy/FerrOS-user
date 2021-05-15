78026527cbd70ea47ad0a4da4f4658f43d61f9ea
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-05-15 21:08:57 +0200

    corrected error in echo

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -13,7 +13,7 @@ use alloc::vec::Vec;
 pub extern "C" fn _start(heap_address: u64, heap_size: u64, args_number: u64, _args: u64) {
     ferr_os_librust::allocator::init(heap_address, heap_size);
     let arguments = ferr_os_librust::env::retrieve_arguments(args_number, _args);
-    main(args);
+    main(arguments);
 }
 
 #[inline(never)]
